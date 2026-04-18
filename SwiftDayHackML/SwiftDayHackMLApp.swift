610cb78e2fb23e21ae5778b14f6fb3648e85f0b5
Daniella y Emilio <daniellaaceituno@gmail.com>
2026-04-17 23:24:20 -0600

    Initial Commit

---
+++ SwiftDayHackML/SwiftDayHackMLApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftDayHackMLApp.swift
+//  SwiftDayHackML
+//
+//  Created by STEM-CEI on 17/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftDayHackMLApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
